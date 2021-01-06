2fd2f47822680370f273b07d33a51cde1a0730fb
dnsdudrla97 <dnsdudrla97@naver.com>
2021-01-06 12:55:42 +0900

    presenter

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -7,7 +7,7 @@ import Message from 'Components/Message';
 import Poster from 'Components/Poster';
 
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 20px;
 `;
 const HomePresenter = ({nowPlaying, popular, upComing, error, loading}) =>
   loading ? (
@@ -17,21 +17,45 @@ const HomePresenter = ({nowPlaying, popular, upComing, error, loading}) =>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map((movie) => (
-            <Poster key={movie.id}/>
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              rating={movie.vote_average}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
       {upComing && upComing.length > 0 && (
         <Section title="upComing movies">
           {upComing.map((movie) => (
-            <Poster key={movie.id}/>
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              rating={movie.vote_average}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="popular movies">
           {popular.map((movie) => (
-            <Poster key={movie.id}/>
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              rating={movie.vote_average}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -4,29 +4,62 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Loader from 'Components/loader';
 import Message from 'Components/Message';
+import Poster from 'Components/Poster';
 
 const Container = styled.div`
   padding: 0px 20px;
 `;
 const TVPresenter = ({topRated, popular, airingToday, error, loading}) =>
-  loading ? <Loader /> : (
+  loading ? (
+    <Loader />
+  ) : (
     <Container>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
-          {topRated.map((show) => show.name)}
+          {topRated.map((show) => (
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+              isMovie={false}
+            />
+          ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="popular Shows">
-          {popular.map((show) => show.name)}
+          {popular.map((show) => (
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+              isMovie={false}
+            />
+          ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title="airingToday Shows">
-          {airingToday.map((show) => show.name)}
+          {airingToday.map((show) => (
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+              isMovie={false}
+            />
+          ))}
         </Section>
       )}
-      {error && <Message color='#e74c3c' text={error}/>}
+      {error && <Message color="#e74c3c" text={error} />}
     </Container>
   );
 
